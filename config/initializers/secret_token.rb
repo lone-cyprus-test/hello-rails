9887e67ff29d109336df6189a87232ef0cfb7b66
raniemi <dev@lone-cyprus.com>
2013-08-20 11:27:00 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = '3fcf8b09951375c3a7070fbf21b8e36d31fc682755a7062b533df4a2c2e06312cd814dcdbebe00b153d5e2f27b35045ca13434d662dfc4e6fa0e445675f9b837'
